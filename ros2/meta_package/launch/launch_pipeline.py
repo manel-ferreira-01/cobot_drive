879bc7b7bab6bfa39540c24483bbd5c84ef26397
Manuel Ferreira <manuel.f.ferreira@tecnico.ulisboa.pt>
2024-05-28 18:55:50 +0100

    feat: added a more seamless way to teleop w/ joystick

--- ros2/meta_package/launch/launch_pipeline.py
+++ ros2/meta_package/launch/launch_pipeline.py
@@ -8,9 +8,24 @@ from ament_index_python.packages import get_package_prefix, get_package_share_di
 
 def generate_launch_description():
     
-    workspace_directory = os.getenv('ROS_WORKSPACE', os.getcwd())    
+    workspace_directory = os.getenv('ROS_WORKSPACE', os.getcwd())  
+    teleop_flag = True 
     
-    return launch.LaunchDescription([
+    teleop_joy_pkgs = launch.LaunchDescription([
+        launch_ros.actions.Node(
+            package='joy_linux',
+            executable='joy_linux_node',
+            name='joy_linux'),
+        
+        launch.actions.IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(get_package_share_directory('teleop_twist_joy')+'/launch/teleop-launch.py'),
+            launch_arguments = {
+                'config_filepath': "/workspace/ros2/meta_package/config/joy.yaml"
+                }.items()
+            )
+    ])
+    
+    launch_list = launch.LaunchDescription([
         launch_ros.actions.Node(
             package='cobot_drive',
             executable='cobot_drive_main',
@@ -26,14 +41,19 @@ def generate_launch_description():
         ),
         launch.actions.IncludeLaunchDescription(
         XMLLaunchDescriptionSource(
-                [os.path.join(
-         get_package_share_directory('rosbridge_server'), 'launch'),
+                [os.path.join(get_package_share_directory('rosbridge_server'), 'launch'),
          '/rosbridge_websocket_launch.xml'])),
         
         launch.actions.IncludeLaunchDescription(
         PythonLaunchDescriptionSource(get_package_share_directory('meta_package')+'/launch/static_tfs.py')),
-        
-    ])
+        ]
+    )
+    
+    if teleop_flag : 
+        secondary_actions = teleop_joy_pkgs.entities
+        launch_list.entities.extend(secondary_actions)
+                
+    return launch_list
 
 if __name__ == '__main__':
     #workspace_directory = os.getenv('ROS_WORKSPACE', os.getcwd()) 
